ece1700b2bc9146d2d393bb6325f2da92e4315c4
kmoutaou0 <kmoutaou0@kmoutaou>
2025-04-07 13:56:35 -0500

    part1 && part2 eth1 interface

--- p1/scripts/install-k3s-server.sh
+++ p1/scripts/install-k3s-server.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 echo "[+] Installing K3s Server..."
 
-curl -sfL https://get.k3s.io | sh -
+echo "Updating system..."
+sudo apt-get update -y
+
+echo "[+] Installing net-tools..."
+sudo apt-get install -y net-tools
+sudo ln -s /sbin/ifconfig /usr/local/bin/ifconfig
+
+sudo ip link add eth1 type dummy && sudo ip addr add 192.168.56.110/24 dev eth1 && sudo ip link set eth1 up
+
+NODE_IP=192.168.56.110
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=${NODE_IP} --bind-address=${NODE_IP} --flannel-iface=eth1" sh -
 
 echo "[+] K3s Server installed."
+
