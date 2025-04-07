804cf7b002fd061fd531c4590723ff2b9909c0ab
Kate-77 <kaoutar.moutaouakil77@gmail.com>
2025-04-07 21:51:33 +0100

    part1 && part2 - add protocol

--- p2/scripts/install-k3s-server.sh
+++ p2/scripts/install-k3s-server.sh
@@ -7,10 +7,10 @@ echo "[+] Installing net-tools"
 sudo apt-get install -y net-tools
 sudo ln -s /sbin/ifconfig /usr/local/bin/ifconfig
 
-sudo ip link add eth1 type dummy && sudo ip addr add 192.168.56.111/24 dev eth1 && sudo ip link set eth1 up
+sudo ip link add eth1 type dummy && sudo ip addr add 192.168.56.110/24 dev eth1 && sudo ip link set eth1 up
 
-echo "Installing K3s (server mode)..."
-curl -sfL https://get.k3s.io | sh -
+NODE_IP=192.168.56.110
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=${NODE_IP} --bind-address=${NODE_IP} --flannel-iface=eth1" sh -
 
 echo "Waiting for K3s to be ready..."
 sleep 20
